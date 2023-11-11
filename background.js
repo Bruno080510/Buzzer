a074f82510dbaf3c6cf863430345c393003e767f
Bruno <brunolealmaciel@gmail.com>
2023-11-10 21:19:19 -0300

    MEUDEUSTAFUNCIONANDO

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+// background.js
+
 console.log(this);
 
 function showNotification(title, message) {
@@ -19,58 +21,47 @@ async function checkSiteSecurity(url) {
 
   try {
     console.log('Detalhes da solicitação:', JSON.stringify({ url }));
-    
+
     const response = await fetch(apiUrl, {
       method: 'POST',
       headers: {
         'x-apikey': apiKey,
         'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: `url=${encodeURIComponent(url)}`, // Ajuste no corpo da solicitação
+      body: `url=${encodeURIComponent(url)}`,
     });
 
     console.log('Resposta da API:', response);
 
     if (response.ok) {
-      console.log("oiii");
       const data = await response.json();
-      const scanId = data.data.id;
+      console.log('Dados da resposta:', JSON.stringify(data));
 
-      // Consultar o resultado da verificação em segundo plano (isso é apenas um exemplo)
-      const result = await checkScanResult(scanId);
-      showNotification("Bora", "Funcionou");
+      // Verifique se o site é seguro com base na nova lógica
+      const isSafe = isSiteSafe(data);
 
-      return result;
+      if (isSafe) {
+        showNotification('Verificação de Segurança Concluída', 'O site é seguro.');
+      } else {
+        showNotification('Verificação de Segurança Concluída', 'O site não é seguro.');
+      }
+    } else {
+      console.error('Erro na verificação de segurança. Status:', response.status);
     }
   } catch (error) {
     console.error('Erro ao verificar a segurança do site:', error);
-    return null;
   }
-
 }
 
-// Função para consultar o resultado da verificação
-async function checkScanResult(scanId) {
-  const apiKey = '842fdcd04948ec11f1a2d3818c093f77d0f718ddf1405d4c7a7010beac58f59a'; // Substitua pela sua chave de API
-  const apiUrl = `https://www.virustotal.com/api/v3/urls/${scanId}`;
+// Função para verificar se o site é seguro
+function isSiteSafe(data) {
+  // Adapte essa lógica de acordo com a estrutura real da resposta
+  // Por exemplo, pode ser que a resposta tenha uma propriedade 'data' e dentro dela uma propriedade 'attributes'
+  const analysisType = data.data.type;
 
-  try {
-    const response = await fetch(apiUrl, {
-      headers: {
-        'x-apikey': apiKey,
-      },
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      const scanResult = data.data.attributes.last_analysis_stats;
-
-      return scanResult;
-    }
-  } catch (error) {
-    console.error('Erro ao consultar o resultado da verificação:', error);
-    return null;
-  }
+  // Verifique se o tipo de análise é o esperado
+  // Adapte conforme necessário com base na estrutura real da resposta
+  return analysisType === 'analysis';
 }
 
 // Listener de eventos para quando uma página é completamente carregada
@@ -79,11 +70,6 @@ chrome.webNavigation.onCompleted.addListener(function(details) {
     const url = details.url;
 
     // Verificar a segurança do site
-    checkSiteSecurity(url)
-      .then(result => {
-        if (result) {
-          showNotification('Verificação de Segurança Concluída', `O site é seguro: ${result.safe} / Não seguro: ${result.suspicious}`);
-        }
-      });
+    checkSiteSecurity(url);
   }
 });
