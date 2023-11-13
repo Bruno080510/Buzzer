2001e74a9f52d7cff0577b13322d3180e9d4e468
Bruno Leal <brunolealmaciel@gmail.com>
2023-11-13 16:47:11 -0300

    teste de remoção do log

--- background.js
+++ background.js
@@ -13,54 +13,52 @@ function showNotification(title, message) {
 }
 
 async function checkSiteSecurity(url) {
-  if (url === "chrome://new-tab-page/" || url.startsWith(toCheck) || url.includes("/confirmation.html")) {
-    console.log("Oii");
-  } else {
-    if (checkedUrls.includes(url)) {
-      console.log('URL já verificada. Ignorando a verificação.');
-      return;
-    }
+  if (url === "chrome://new-tab-page/" || url.startsWith(toCheck) || url.includes("/confirmation.html")) return;
 
-    const apiKey = '842fdcd04948ec11f1a2d3818c093f77d0f718ddf1405d4c7a7010beac58f59a';
-    const apiUrl = `https://www.virustotal.com/vtapi/v2/url/report?apikey=${apiKey}&resource=${encodeURIComponent(url)}`;
+  if (checkedUrls.includes(url)) {
+    console.log('URL já verificada. Ignorando a verificação.');
+    return;
+  }
 
-    console.log('Iniciando verificação de segurança para:', url);
+  const apiKey = '842fdcd04948ec11f1a2d3818c093f77d0f718ddf1405d4c7a7010beac58f59a';
+  const apiUrl = `https://www.virustotal.com/vtapi/v2/url/report?apikey=${apiKey}&resource=${encodeURIComponent(url)}`;
 
-    try {
-      const response = await fetch(apiUrl);
-      console.log('Detalhes da solicitação:', JSON.stringify({ url }));
-      console.log('Resposta da API:', response);
+  console.log('Iniciando verificação de segurança para:', url);
 
-      if (response.ok) {
-        const data = await response.json();
-        console.log('Dados da resposta:', (data));
+  try {
+    const response = await fetch(apiUrl);
+    console.log('Detalhes da solicitação:', JSON.stringify({ url }));
+    console.log('Resposta da API:', response);
 
-        const isSafe = isSiteSafe(data);
+    if (response.ok) {
+      const data = await response.json();
+      console.log('Dados da resposta:', (data));
 
-        if (isSafe == 0) {
-          showNotification('Verificação de Segurança Concluída', 'O site é seguro.');
-        } else {
-          console.log('Site não seguro. Armazenando URL no chrome.storage.local.');
-          chrome.storage.local.set({ 'currentUrl': url }, function() {
-            console.log('URL armazenada com sucesso no chrome.storage.local.');
-          });
-          showNotification('Verificação de Segurança Concluída', 'O site não é seguro.');
+      const isSafe = isSiteSafe(data);
 
-          // Adicione a verificação para evitar redirecionar se já estiver na página de confirmação
-          if (!url.includes("/confirmation.html")) {
-            chrome.tabs.update({ url: '/confirmation.html' });
-          }
-
-          checkedUrls.push(url);
-
-          return;
-        }
+      if (isSafe == 0) {
+        showNotification('Verificação de Segurança Concluída', 'O site é seguro.');
       } else {
-        console.error('Erro na verificação de segurança. Status:', response.status);
+        console.log('Site não seguro. Armazenando URL no chrome.storage.local.');
+        chrome.storage.local.set({ 'currentUrl': url }, function() {
+          console.log('URL armazenada com sucesso no chrome.storage.local.');
+        });
+        showNotification('Verificação de Segurança Concluída', 'O site não é seguro.');
+
+        // Adicione a verificação para evitar redirecionar se já estiver na página de confirmação
+        if (!url.includes("/confirmation.html")) {
+          chrome.tabs.update({ url: '/confirmation.html' });
+        }
+
+        checkedUrls.push(url);
+
+        return;
       }
-    } catch (error) {
-      console.error('Erro ao verificar a segurança do site:', error);
+    } else {
+      console.error('Erro na verificação de segurança. Status:', response.status);
     }
+  } catch (error) {
+    console.error('Erro ao verificar a segurança do site:', error);
   }
 }
 
